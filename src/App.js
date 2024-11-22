f44e472a261a18b955e3989c2a6642d727eeb5cb
plusmail <plus4957@gmail.com>
2024-11-22 16:56:09 +0900

    dddd

--- src/App.js
+++ src/App.js
@@ -1,29 +1,77 @@
 import TodoTemplate from "./TodoTemplate";
 import TodoInsert from "./TodoInsert";
 import TodoList from "./TodoList";
-import {useCallback, useRef, useState} from "react";
+import {useCallback, useReducer, useRef, useState} from "react";
+
+// const complexObject = {
+//     id:1,
+//     object: {
+//         k:11,
+//         m:21
+//     },
+//     name : "홍길동"
+// }
+//
+// const nextComplexObject = {
+//     ...complexObject,
+//     object : {
+//         ...complexObject.k,
+//         k:100
+//     }
+// }
+
+function  createBulkTodos(){
+    const array = [];
+    for(let i=1; i<=2500; i++){
+        array.push({
+            id:i,
+            text: `할 일.... ${i}`,
+            checked: false
+        })
+    }
+    return array;
+}
+
+function todoReducer(todos, action){
+    switch (action.type) {
+        case 'INSERT':
+            return todos.concat(action.todo);
+        case 'REMOVE':
+            return  todos.filter( todo => todo.id !== action.id);
+        case 'TOGGLE':
+            return todos.map( todo =>
+                todo.id === action.id ? {...todo, checked: !todo.checked} : todo,
+            );
+        default:
+            return  todos;
+    }
+}
 
 function App() {
-    const [todos, setTodos] = useState([
-        {
-            id: 1,
-            text: '스프링 boot',
-            checked: true,
-        },
-        {
-            id: 2,
-            text: '컴포넌트 배오기',
-            checked: true,
-        },
-        {
-            id: 3,
-            text: '일정관리 앱 만들기',
-            checked: false,
-        }
+    // const [todos, setTodos] = useState([
+    //     {
+    //         id: 1,
+    //         text: '스프링 boot',
+    //         checked: true,
+    //     },
+    //     {
+    //         id: 2,
+    //         text: '컴포넌트 배오기',
+    //         checked: true,
+    //     },
+    //     {
+    //         id: 3,
+    //         text: '일정관리 앱 만들기',
+    //         checked: false,
+    //     }
+    //
+    // ])
 
-    ])
+    // const [todos, setTodos] = useState(createBulkTodos)
 
-    const nextId = useRef(4);
+    const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+
+    const nextId = useRef(2501);
 
     const onInsert = useCallback(
         // text 는 TodoInsert onSubmit에 있는 onInsert( value )
@@ -33,18 +81,38 @@ function App() {
                 text,
                 checked: false
             };
-            setTodos(todos.concat(todo))
+            // setTodos(todos.concat(todo))
+            dispatch( {type: 'INSERT', todo});
             nextId.current += 1
         },
-        [todos],
+        [],
     );
 
+    const onRemove = useCallback(
+        id=>{
+            // setTodos(todos.filter(
+            //     todo=> todo.id !== id
+            // ))
+            dispatch({type:'REMOVE', id});
+        },[]
+    )
+
+    const onToggle = useCallback(
+        id=>{
+            // setTodos(
+            //     todos.map(todo =>
+            //         todo.id === id ? {...todo, checked : !todo.checked } : todo
+            //     )
+            // )
+            dispatch({type:'TOGGLE', id})
+        }, []
+    )
 
     return (
         <TodoTemplate>
             {/*App.js에서 onInsert useCallback 함수를 TodoInsert로 함수를 전달*/}
             <TodoInsert onInsert={onInsert}/>
-            <TodoList todos={todos}/>
+            <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
         </TodoTemplate>
     );
 }
--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,22 +1,30 @@
 import {MdCheckBox, MdCheckBoxOutlineBlank, MdRemoveCircleOutline} from "react-icons/md";
 import '@styles/TodoListItem.scss';
 import cn from 'classnames';
+import React from 'react';
 
-const TodoListItem = ({todo})=>{
+const TodoListItem = ({todo, onRemove, onToggle, style})=>{
 
-    const { text, checked} = todo;
+    const { id, text, checked} = todo;
 
     return (
-        <div className={"TodoListItem"}>
-            <div className={cn('checkbox', {checked})}>
-                {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/>}
-                <div className={"text"}>{text}</div>
+        <div className={"TodoListItem-virtualized"} style={style}>
+
+            <div className={"TodoListItem"}>
+                <div
+                    className={cn('checkbox', {checked})}
+                    onClick={() => onToggle(id)}
+                >
+                    {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/>}
+                    <div className={"text"}>{text}</div>
+                </div>
+                <div className={"remove"} onClick={() => onRemove(id)}>
+                    <MdRemoveCircleOutline/>
+                </div>
             </div>
-            <div className={"remove"}>
-                <MdRemoveCircleOutline/>
-            </div>
+
         </div>
     )
 }
 
-export default TodoListItem;
+export default React.memo(TodoListItem);
